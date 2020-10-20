eb770dc8c7737ccf481e4281866690892d44d58d
Alptahta <alp.tahta@protonmail.com>
2020-10-20 15:43:55 +0300

    Create project structure

---
+++ src/main/java/com/example/LibraryAppDemo/LibraryAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibraryAppDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LibraryAppDemo/LibraryAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibraryAppDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
