b39de4e416fcf2dfddf7c2e63aeae80820c37288
Alptahta <alp.tahta@protonmail.com>
2020-10-21 22:32:20 +0300

    Create Entity Objects

---
+++ src/main/java/com/example/LibraryAppDemo/Entity/Author.java
@@ -0,0 +1,80 @@
+package com.example.LibraryAppDemo.Entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "author")
+public class Author implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(mappedBy = "author")
+    private Set<Book> books = new HashSet<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author)) return false;
+        Author author = (Author) o;
+        return id.equals(author.id) &&
+                getName().equals(author.getName()) &&
+                getDescription().equals(author.getDescription()) &&
+                getBooks().equals(author.getBooks());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, getName(), getDescription(), getBooks());
+    }
+}
---
+++ src/main/java/com/example/LibraryAppDemo/Entity/Publisher.java
@@ -0,0 +1,79 @@
+package com.example.LibraryAppDemo.Entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "publisher")
+public class Publisher implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @Column(name = "name")
+    @NotNull
+    private String name;
+
+    @Column(name = "description")
+    @NotNull
+    private String description;
+
+    @OneToMany(mappedBy = "publisher")
+    private Set<Book> books = new HashSet<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Publisher)) return false;
+        Publisher publisher = (Publisher) o;
+        return id.equals(publisher.id) &&
+                getName().equals(publisher.getName()) &&
+                getDescription().equals(publisher.getDescription()) &&
+                getBooks().equals(publisher.getBooks());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, getName(), getDescription(), getBooks());
+    }
+}
